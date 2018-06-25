7305bc6e99b065df1ef67194eb96a3d1fcb4ae7c
DrMayx <mj6367@gmail.com>
2018-06-25 18:43:42 +0200

    Create main ServerController

--- src/main/java/server/ServerController.java
+++ src/main/java/server/ServerController.java
@@ -1,4 +1,25 @@
 package server;
 
 public class ServerController {
+
+    public static void main(String[] args) {
+
+        try{
+            switch(args[0]){
+                case "server":
+                    System.out.println("Logged as SERVER at port: " + args[1]);
+                    // Launch server and start listening for new clients
+                    break;
+                case "client":
+                    System.out.println("Logged as " + args[1] + " at port: " + args[2]);
+                    // Launch client if server is working and start listening for output and input. If server is not working print message and exit the program.
+                    break;
+                default:
+                    System.out.println("Invalid mode!");
+                    break;
+            }
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Please enter correct mode : server OR client [username] [port]");
+        }
+    }
 }
